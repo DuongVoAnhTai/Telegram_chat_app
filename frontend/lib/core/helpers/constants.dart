bef6cb519d722f1228e114e69e5387d19481ca70
Harry Nguyen <nguyenvuong171104@gmail.com>
2025-03-28 16:28:24 +0700

    feat: add constanst
    dùng để định nghĩa các loại màu sắc, size,...

---
+++ frontend/lib/core/helpers/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// File: lib/constants.dart
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFEDF2F6); // Màu nền Telegram
+  static const Color primaryColor = Color(0xFF0088CC); // Màu xanh chủ đạo
+  static const Color white = Colors.white;
+  static const Color textPrimary = Colors.black87;
+  static const Color textSecondary = Color(0xFF757575); // Màu xám cho text phụ
+}
+
+class AppSizes {
+  static const double padding = 32.0; // Khoảng cách padding mặc định
+  static const double buttonHeight = 50.0; // Chiều cao nút
+  static const double iconSizeLarge = 100.0; // Kích thước logo
+  static const double fontSizeTitle = 32.0; // Cỡ chữ tiêu đề
+  static const double fontSizeBody = 16.0; // Cỡ chữ nội dung
+  static const double fontSizeButton = 18.0; // Cỡ chữ nút
+  static const double borderRadius = 10.0; // Độ bo góc
+  static const double spacingLarge = 50.0; // Khoảng cách lớn
+  static const double spacingMedium = 30.0; // Khoảng cách trung bình
+  static const double spacingSmall = 20.0; // Khoảng cách nhỏ
+}
